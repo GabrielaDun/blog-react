4584d3d37692e24e80482d41f2abd49cabe805b2
GabrielaDun <123959816+GabrielaDun@users.noreply.github.com>
2023-07-28 11:48:06 +0200

    Prepered for publishing

--- blog-react/src/redux/categoriesRedux.js
+++ blog-react/src/redux/categoriesRedux.js
@@ -1,24 +1,12 @@
-//variables
-const ADD_CATEGORY = 'app/categories/ADD_CATEGORY';
 //selectors
 export const getCategories = state => state.categories;
 export const getCategoryById = ({posts}, category) => 
 posts.filter(post => post.category.toLowerCase() === category.toLowerCase())
 
-// action creators
-export const addCategory = (payload) => ({
-  type: ADD_CATEGORY,
-  payload
-})
 
-
-const categoriesReducer = (statePart = [], action) => {
-  switch (action.type) {
-    case ADD_CATEGORY:
-      return [...statePart, action.payload];
-    default:
-      return statePart;
-  }
+const categoriesReducer = (statePart = []) => {
+  return statePart;
+  
 };
 
 export default categoriesReducer;
